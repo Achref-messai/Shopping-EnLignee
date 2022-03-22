c4743779703a455e5b1a537975ae01ebba99dea7
Achref Messai <messai.achref123@gmail.com>
2022-03-22 20:54:38 +0100

    firt

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Registre from "./components/Registre/Registre";
+import Login from "./components/Login/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/Register" /> : <Registre />}
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.colorjetgroup.com/upload/blog/tshirt-printing-sample.jpg",
+    title: "SUMMER",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.footlocker.com/is/image/FLEU/314103901204_01?wid=520&hei=520&fmt=png-alpha",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://cdn.flightclub.com/TEMPLATE/253591/1.jpg",
+    title: "SPORT SHOES",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://p4.wallpaperbetter.com/wallpaper/320/139/825/men-model-black-and-white-man-sunglasses-hd-wallpaper-preview.jpg",
+    title: "SHIRT STYLE!",
+    cat: "men",
+  },
+  {
+    id: 2,
+    img: "https://p4.wallpaperbetter.com/wallpaper/980/303/685/anyutka-mikhaylova-women-model-wallpaper-preview.jpg",
+    title: "LOUNGEWEAR LOVE",
+    cat: "femme",
+  },
+  {
+    id: 3,
+    img: "https://gentlemanmoderne.com/wp-content/uploads/2020/06/accessoires-indispensables-homme-bracelets-bijoux.jpg",
+    title: "ACCESSOIRES ",
+    cat: "accesoires",
+  },
+];
